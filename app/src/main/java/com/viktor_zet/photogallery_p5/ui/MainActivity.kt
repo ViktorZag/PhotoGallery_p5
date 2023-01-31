c3aefae8effbd579f463b67b7ad96267f745afce
Viktor <viktorzahurskiy@gmail.com>
2023-01-31 11:55:34 +0200

    develop-1 Created blank Fragment wired up to Activity

---
+++ app/src/main/java/com/viktor_zet/photogallery_p5/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.viktor_zet.photogallery_p5.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.viktor_zet.photogallery_p5.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

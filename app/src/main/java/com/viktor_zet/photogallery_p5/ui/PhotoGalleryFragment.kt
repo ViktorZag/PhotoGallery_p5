6f183ca9dc3e3cb15f5c8c52608dac190fccb323
Viktor <viktorzahurskiy@gmail.com>
2023-02-15 00:09:51 +0200

    develop-6 Implemented ToolBar, added search feature

--- app/src/main/java/com/viktor_zet/photogallery_p5/ui/PhotoGalleryFragment.kt
+++ app/src/main/java/com/viktor_zet/photogallery_p5/ui/PhotoGalleryFragment.kt
@@ -2,14 +2,14 @@ package com.viktor_zet.photogallery_p5.ui
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.viktor_zet.photogallery_p5.R
 import com.viktor_zet.photogallery_p5.databinding.FragmentPhotoGalleryBinding
 import com.viktor_zet.photogallery_p5.repository.PhotoRepository
 import kotlinx.coroutines.flow.collect
@@ -25,6 +25,11 @@ class PhotoGalleryFragment : Fragment() {
             "Cannot access binding because it is null. Is the view visible?"
         }
     private val photoGalleryViewModel: PhotoGalleryViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -48,6 +53,29 @@ class PhotoGalleryFragment : Fragment() {
 
 
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.fragment_photo_gallery, menu)
+
+        val searchItem: MenuItem = menu.findItem(R.id.menu_item_search)
+        val searchView = searchItem.actionView as? SearchView
+
+        searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Log.d(TAG, "QueryTextSubmit: $query")
+                photoGalleryViewModel.setQuery(query ?: "")
+                searchView.clearFocus()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                Log.d(TAG, "QueryTextChange: $newText")
+                return false
+            }
+
+        })
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/viktor_zet/photogallery_p5/ui/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/viktor_zet/photogallery_p5/ui/PhotoGalleryViewModel.kt
@@ -23,11 +23,21 @@ class PhotoGalleryViewModel : ViewModel() {
     init {
         viewModelScope.launch {
             try {
-                val items = photoRepository.searchPhotos("ukraine")
+                val items = fetchGalleryItems("")
                 _galleryItems.value = items
             } catch (ex: Exception) {
                 Log.e(TAG, "Failed to fetch gallery items", ex)
             }
         }
+    }
+
+    fun setQuery(query: String) {
+        viewModelScope.launch {_galleryItems.value = fetchGalleryItems(query) }
+    }
+
+    private suspend fun fetchGalleryItems(query: String): List<GalleryItem> {
+        return if (query.isNotEmpty()) {
+            photoRepository.searchPhotos(query)
+        } else photoRepository.fetchPhotos()
     }
 }

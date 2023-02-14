ee7d8d0eb65ebd91470029b8e20f5a3095021a0e
Viktor <viktorzahurskiy@gmail.com>
2023-02-14 16:49:56 +0200

    develop-5 Added interceptor and backend for search feature

--- app/src/main/java/com/viktor_zet/photogallery_p5/api/FlickrApi.kt
+++ app/src/main/java/com/viktor_zet/photogallery_p5/api/FlickrApi.kt
@@ -2,15 +2,15 @@ package com.viktor_zet.photogallery_p5.api
 
 import com.viktor_zet.photogallery_p5.model.FlickrResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 private const val API_KEY = "a271fba6aab628f0c586b214ba3e625a"
 
 interface FlickrApi {
 
-    @GET("services/rest/?method=flickr.interestingness.getList"+
-    "&api_key=$API_KEY"+
-    "&format=json"+
-    "&nojsoncallback=1"+
-    "&extras=url_s")
+    @GET("services/rest/?method=flickr.interestingness.getList")
     suspend fun fetchPhotos(): FlickrResponse
+
+    @GET("services/rest?method=flickr.photos.search")
+    suspend fun searchPhotos(@Query("text") query: String): FlickrResponse
 }
--- app/src/main/java/com/viktor_zet/photogallery_p5/repository/PhotoRepository.kt
+++ app/src/main/java/com/viktor_zet/photogallery_p5/repository/PhotoRepository.kt
@@ -1,7 +1,9 @@
 package com.viktor_zet.photogallery_p5.repository
 
 import com.viktor_zet.photogallery_p5.api.FlickrApi
+import com.viktor_zet.photogallery_p5.api.PhotoInterceptor
 import com.viktor_zet.photogallery_p5.model.GalleryItem
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.create
@@ -11,12 +13,19 @@ class PhotoRepository {
     private val flickrApi: FlickrApi
 
     init {
+        val okHttpClient =  OkHttpClient.Builder()
+            .addInterceptor(PhotoInterceptor())
+            .build()
+
         val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl("https://api.flickr.com/")
             .addConverterFactory(MoshiConverterFactory.create())
+            .client(okHttpClient)
             .build()
         flickrApi = retrofit.create()
     }
 
     suspend fun fetchPhotos(): List<GalleryItem> = flickrApi.fetchPhotos().photos.galleryItems
+
+    suspend fun searchPhotos(query : String) : List<GalleryItem> = flickrApi.searchPhotos(query).photos.galleryItems
 }
